27aabe28fa397f42c5b2ce63503fa32d54a12abf
Dhanya-bhat <53563154+Dhanya-bhat@users.noreply.github.com>
2019-09-13 15:49:54 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,24 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{int n;cin>>n;
-int *arr=new int[n];
-int temp;
-for(int i=0; i<n; i++){
-cin>>temp;arr[i]=temp;}
-sort(arr,arr+n);
-for(int i=1; i<n; i++)
-{if(arr[i]-arr[i-1]>0)
-{arr[i-1]=0;}
-else
-{int j=i-1;
-while(j>=0){
-if(arr[j]!=0&&arr[i]-arr[j]>0){
-arr[j]=0;break;}
-else
-j--;}}}
-int sum=0; for(int i=0; i<n; i++) {if(arr[i]!=0)
-sum=sum+arr[i];
+#include <stdio.h>
+int frusted_coders(int n, int k)
+{ 
+ if (n == 1) 
+ return 1; 
+  else
+   return (frusted_coders(n - 1, k) + k-1) % n + 1; 
+} 
+int main() 
+{ 
+ int n = 14; 
+ int k = 2; 
+ printf("The chosen place is %d",frusted_coders(n, k)); 
+ return 0;
 }
-cout<<sum;
-return 0;}
